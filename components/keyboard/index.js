85979796d9a7b15c6dc0a6fcfdad580dfeb64a77
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-05 22:10:58 -0400

    color changes for keycaps

--- components/keyboard/index.js
+++ components/keyboard/index.js
@@ -53,7 +53,7 @@ const Key = ({ size = 1 }) => {
 				h={15}
 				r={-90}
 				tz={13.5}
-				bgcolor="#838383"
+				bgcolor="#A38C5F"
 			/>
 			<KeyFaceX
 				className="key-back"
@@ -61,7 +61,7 @@ const Key = ({ size = 1 }) => {
 				h={15}
 				r={90}
 				tz={13.5}
-				bgcolor="#fffffb"
+				bgcolor="#EDE4C7"
 			/>
 			<KeyFaceY
 				className="key-right"
@@ -69,7 +69,7 @@ const Key = ({ size = 1 }) => {
 				h={27}
 				r={90}
 				tz={15 * size}
-				bgcolor="#838383"
+				bgcolor="#A38C5F"
 			/>
 			<KeyFaceY
 				className="key-left"
@@ -77,7 +77,7 @@ const Key = ({ size = 1 }) => {
 				h={27}
 				r={90}
 				tz={-15 * size}
-				bgcolor="#b8b8b"
+				bgcolor="#b8b8b8"
 			/>
 			<KeyFaceY
 				className="key-top"
@@ -85,8 +85,8 @@ const Key = ({ size = 1 }) => {
 				h={27}
 				r={0}
 				tz={7.5}
-				bgcolor="#fffffb"
-				backgroundImage="linear-gradient(to bottom, #f4fbff, #fffffb)"
+				bgcolor="#EDE4C7"
+				backgroundImage="linear-gradient(to bottom, #f4fbff, #EDE4C7)"
 			/>
 			<KeyFaceY
 				className="key-bottom"
@@ -94,7 +94,7 @@ const Key = ({ size = 1 }) => {
 				h={27}
 				r={180}
 				tz={7.5}
-				bgcolor="#838383"
+				bgcolor="#A38C5F"
 			/>
 		</Box>
 	)
@@ -122,7 +122,7 @@ const Key2 = ({ size, altColor = false }) => {
 				h={15}
 				r={-90}
 				tz={13.5}
-				bgcolor={altColor ? '#8e3e43' : '#426585'}
+				bgcolor={altColor ? '#5D2C26' : '#242328'}
 			/>
 			<KeyFaceX
 				className="key-back"
@@ -130,7 +130,7 @@ const Key2 = ({ size, altColor = false }) => {
 				h={15}
 				r={90}
 				tz={13.5}
-				bgcolor={altColor ? '#ffa28e' : '#a9dffd'}
+				bgcolor={altColor ? '#946062' : '#7B7582'}
 			/>
 			<KeyFaceY
 				className="key-right"
@@ -138,7 +138,7 @@ const Key2 = ({ size, altColor = false }) => {
 				h={27}
 				r={90}
 				tz={15 * size}
-				bgcolor={altColor ? '#ffa28e' : '#a9dffd'}
+				bgcolor={altColor ? '#946062' : '#4A474E'}
 			/>
 			<KeyFaceY
 				className="key-left"
@@ -146,12 +146,12 @@ const Key2 = ({ size, altColor = false }) => {
 				h={27}
 				r={90}
 				tz={-15 * size}
-				bgcolor={altColor ? '#8e3e43' : '#426585'}
-				bgcolor="#b8b8b"
+				bgcolor={altColor ? '#5D2C26' : '#242328'}
+				bgcolor="#b8b8b8"
 				backgroundImage={
 					altColor
 						? 'linear-gradient(to bottom, #bc7377, #b46266)'
-						: 'linear-gradient(to bottom, #a7dcfa, #8fd2f8)'
+						: 'linear-gradient(to bottom, ##9F97A8, #7B7582)'
 				}
 			/>
 			<KeyFaceY
@@ -160,7 +160,7 @@ const Key2 = ({ size, altColor = false }) => {
 				h={27}
 				r={0}
 				tz={7.5}
-				bgcolor={altColor ? '#ffa28e' : '#a9dffd'}
+				bgcolor={altColor ? '#946062' : '#7B7582'}
 			/>
 			<KeyFaceY
 				className="key-bottom"
@@ -168,12 +168,12 @@ const Key2 = ({ size, altColor = false }) => {
 				h={27}
 				r={180}
 				tz={7.5}
-				bgcolor={altColor ? '#8e3e43' : '#426585'}
-				bgcolor="#838383"
+				bgcolor={altColor ? '#5D2C26' : '#242328'}
+				bgcolor="#A38C5F"
 				backgroundImage={
 					altColor
 						? 'linear-gradient(to bottom, #bc7377, #b46266)'
-						: 'linear-gradient(to bottom, #a7dcfa, #8fd2f8)'
+						: 'linear-gradient(to bottom, #9F97A8, #7B7582)'
 				}
 			/>
 		</Box>
@@ -207,163 +207,153 @@ const Keyboard = (props) => {
 
 	return (
 		<Box
+			component="div"
+			className="main"
 			sx={{
-				position: 'absolute',
+				'*, *::after, *::before': {
+					m: 0,
+					p: 0,
+					'box-sizing': 'border-box',
+					'transform-style': 'preserve-3d',
+				},
+				position: 'relative',
 				scale: `${props.scale}`,
 				display: { xs: 'none', md: 'flex' },
-				top: {
-					md: 'calc(100vw * .34)',
-					lg: 'calc(100vw * .33)',
-					xl: 'calc(100vw * .32)',
-				},
-				left: -120,
-				right: 0,
-				width: 'auto',
+				m: 'auto',
+				height: 'auto',
+				width: 0,
+
+				//flex
+				justifyContent: 'center',
+				alignItems: 'center',
 			}}
 		>
 			<Box
 				component="div"
-				className="main"
+				className="keyboard"
 				sx={{
-					m: '0 auto',
-					width: '550px',
-					height: '200px',
-					position: 'relative',
-					//flex
+					border: '1px solid green',
+					height: 'auto',
+					width: 0,
+					// change perspective
+					transform:
+						'perspective(2000px) rotateX(75deg) rotateY(0deg) rotateZ(12deg)',
 					display: 'flex',
 					justifyContent: 'center',
 					alignItems: 'center',
-					'*, *::after, *::before': {
-						m: 0,
-						p: 0,
-						'box-sizing': 'border-box',
-						'transform-style': 'preserve-3d',
-					},
+					position: 'absolute',
+					left: -80,
+					bottom: -230,
 				}}
 			>
 				<Box
 					component="div"
-					className="keyboard"
+					className="keyboard-front"
+					sx={{
+						width: '500px',
+						height: '25px',
+						transform: 'rotateX(-90deg) translateZ(80px)',
+						bgcolor: '#BCBCBC',
+						position: 'absolute',
+					}}
+				/>
+				<Box
+					component="div"
+					className="keyboard-back"
+					sx={{
+						width: '500px',
+						height: '25px',
+						transform: 'rotateX(90deg) translateZ(80px)',
+						bgcolor: '#ADADAD',
+						position: 'absolute',
+					}}
+				/>
+				<Box
+					component="div"
+					className="keyboard-right"
+					sx={{
+						width: '25px',
+						height: '160px',
+						transform: 'rotateY(90deg) translateZ(250px)',
+						bgcolor: '#ADADAD',
+						position: 'absolute',
+					}}
+				/>
+				<Box
+					component="div"
+					className="keyboard-left"
+					sx={{
+						width: '25px',
+						height: '160px',
+						transform: 'rotateY(90deg) translateZ(-250px)',
+						bgcolor: '#BCBCBC',
+						position: 'absolute',
+					}}
+				/>
+				<Box
+					component="div"
+					className="keyboard-top"
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-around',
+						width: '500px',
+						height: '160px',
+						transform: 'rotateY(0deg) translateZ(12.5px)',
+						backgroundImage: 'linear-gradient(to bottom, #dbf2fe, #343232)',
+						//face
+						position: 'absolute',
+					}}
+				>
+					{/* row 1 */}
+					<KeyRow>
+						<Key2 size={1} altColor={true} />
+						{defaultKeys(12)}
+						<Key2 size={2} />
+					</KeyRow>
+					{/* row 2 */}
+					<KeyRow>
+						<Key2 size={2} />
+						{defaultKeys(12)}
+						<Key2 size={1} />
+					</KeyRow>
+					{/* row 3 */}
+					<KeyRow>
+						<Key2 size={2} />
+						{defaultKeys(10)}
+						<Key2 size={3} altColor={true} />
+					</KeyRow>
+					{/* row 4 */}
+					<KeyRow>
+						<Key2 size={3} />
+						{defaultKeys(10)}
+						<Key2 size={2} />
+					</KeyRow>
+					{/* row 5 */}
+					<KeyRow>
+						<Key2 size={1} />
+						<Key2 size={1} altColor={true} />
+						<Key2 size={1} />
+						<Key size={7} />
+						<Key2 size={2} />
+						<Key2 size={1} />
+						<Key2 size={1} />
+						<Key2 size={1} />
+					</KeyRow>
+				</Box>
+				<Box
+					component="div"
+					className="keyboard-bottom"
 					sx={{
 						width: '500px',
 						height: '160px',
-						// change perspective
-						transform:
-							'perspective(10000px) rotateX(75deg) rotateY(-5.7deg) rotateZ(20deg)',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
+						transform: 'rotateY(180deg) translateZ(12.5px)',
+						bgcolor: '#eae7e5',
+						position: 'absolute',
+						boxShadow:
+							'-10px 30px 5px rgb(28, 28, 28, .5), 0px 30px 5px rgb(28, 28, 28, .5), 0px 0px 5px rgb(28, 28, 28, .5), 0px 30px 5px rgb(28, 28, 28, .5)',
 					}}
-				>
-					<Box
-						component="div"
-						className="keyboard-front"
-						sx={{
-							width: '500px',
-							height: '25px',
-							transform: 'rotateX(-90deg) translateZ(80px)',
-							bgcolor: '#9c9c9c',
-							position: 'absolute',
-						}}
-					/>
-					<Box
-						component="div"
-						className="keyboard-back"
-						sx={{
-							width: '500px',
-							height: '25px',
-							transform: 'rotateX(90deg) translateZ(80px)',
-							bgcolor: '#fffffb',
-							position: 'absolute',
-						}}
-					/>
-					<Box
-						component="div"
-						className="keyboard-right"
-						sx={{
-							width: '25px',
-							height: '160px',
-							transform: 'rotateY(90deg) translateZ(250px)',
-							bgcolor: '#fffffb',
-							position: 'absolute',
-						}}
-					/>
-					<Box
-						component="div"
-						className="keyboard-left"
-						sx={{
-							width: '25px',
-							height: '160px',
-							transform: 'rotateY(90deg) translateZ(-250px)',
-							bgcolor: '#eae7e5',
-							position: 'absolute',
-						}}
-					/>
-					<Box
-						component="div"
-						className="keyboard-top"
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'space-around',
-							width: '500px',
-							height: '160px',
-							transform: 'rotateY(0deg) translateZ(12.5px)',
-							backgroundImage: 'linear-gradient(to bottom, #dbf2fe, #343232)',
-							//face
-							position: 'absolute',
-						}}
-					>
-						{/* row 1 */}
-						<KeyRow>
-							<Key2 size={1} />
-							{defaultKeys(12)}
-							<Key2 size={2} />
-						</KeyRow>
-						{/* row 2 */}
-						<KeyRow>
-							<Key2 size={2} />
-							{defaultKeys(12)}
-							<Key2 size={1} />
-						</KeyRow>
-						{/* row 3 */}
-						<KeyRow>
-							<Key2 size={2} />
-							{defaultKeys(10)}
-							<Key2 size={3} altColor={true} />
-						</KeyRow>
-						{/* row 4 */}
-						<KeyRow>
-							<Key2 size={3} />
-							{defaultKeys(10)}
-							<Key2 size={2} />
-						</KeyRow>
-						{/* row 5 */}
-						<KeyRow>
-							<Key2 size={1} />
-							<Key2 size={1} altColor={true} />
-							<Key2 size={1} />
-							<Key size={7} />
-							<Key2 size={2} />
-							<Key2 size={1} />
-							<Key2 size={1} />
-							<Key2 size={1} />
-						</KeyRow>
-					</Box>
-					<Box
-						component="div"
-						className="keyboard-bottom"
-						sx={{
-							width: '500px',
-							height: '160px',
-							transform: 'rotateY(180deg) translateZ(12.5px)',
-							bgcolor: '#eae7e5',
-							position: 'absolute',
-							'box-shadow':
-								'-20px 40px 0 #0d0c0c, 0px 40px 0 #0d0c0c, 5px 0px 0 #0d0c0c, 5px 40px 0 #0d0c0c',
-						}}
-					/>
-				</Box>
+				/>
 			</Box>
 		</Box>
 	)
