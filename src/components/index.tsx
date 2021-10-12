baf05c5a2a6a8315fa57a72999192c399fd25de7
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-12 00:58:38 -0400

    add display

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
-export { default as Monitor } from './monitor'
 export { default as Desk } from './desk'
+export { default as Display } from './display'
 export { default as Keyboard } from './keyboard'
+export { default as Monitor } from './monitor'
