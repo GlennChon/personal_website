b274d91feba3ea31e2ae53be3d28db9fe2dee5da
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-13 22:34:15 -0400

    rm unused theme stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
-import Head from 'next/head'
-import type { AppProps /*, AppContext */ } from 'next/app'
-import { createContext, useState, useMemo } from 'react'
-import { ThemeProvider } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material'
 import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
 import createEmotionCache from '../../styles/createEmotionCache'
-import {
-	createTheme,
-	responsiveFontSizes,
-	useTheme,
-} from '@mui/material/styles'
-import '/styles/globals.css'
-import { getDesignTokens } from '../../styles/theme'
+import { theme } from '../../styles/theme'
 import { ViewportProvider } from '../utils/ViewportProvider'
+import '/styles/globals.css'
 
 interface MyAppProps extends AppProps {
 	emotionCache?: EmotionCache
@@ -22,35 +16,20 @@ const clientSideEmotionCache = createEmotionCache()
 //test
 function MyApp(props: MyAppProps) {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
-	const ColorModeContext = createContext({ toggleColorMode: () => {} })
-	const [mode, setMode] = useState('light')
-	const colorMode = useMemo(
-		() => ({
-			toggleColorMode: () => {
-				setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
-			},
-		}),
-		[],
-	)
 
-	const theme = useMemo(
-		() => responsiveFontSizes(createTheme(getDesignTokens(mode))),
-		[mode],
-	)
 	return (
 		<CacheProvider value={emotionCache}>
 			<Head>
 				<title>Glenn Chon</title>
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
 			</Head>
-			<ColorModeContext.Provider value={colorMode}>
-				<ThemeProvider theme={theme}>
-					<ViewportProvider>
-						<CssBaseline />
-						<Component {...pageProps} />
-					</ViewportProvider>
-				</ThemeProvider>
-			</ColorModeContext.Provider>
+
+			<ThemeProvider theme={theme}>
+				<ViewportProvider>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ViewportProvider>
+			</ThemeProvider>
 		</CacheProvider>
 	)
 }
