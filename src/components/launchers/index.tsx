85a5f887e713e0f2bcc0aaab5f097795f09809ce
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-13 22:33:35 -0400

    created launcher flex create

---
+++ src/components/launchers/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+import IconLauncher from './iconLauncher'
+
+const Launchers = ({ itemArr = [] }) => {
+	const clickableIcons = () => {
+		let iconLaunchers = []
+		itemArr.map((item, i) =>
+			iconLaunchers.push(
+				<IconLauncher
+					key={i}
+					icon={item.icon}
+					label={item.label}
+					onClick={item.onClick}
+					sx={{
+						flex: `0 0 auto`,
+						borderRadius: { xs: '15%', sm: 1 },
+					}}
+				/>,
+			),
+		)
+		return iconLaunchers
+	}
+
+	return (
+		<>
+			<Box
+				component="div"
+				className="desktop-display-container"
+				sx={{
+					height: '100%',
+					width: '100%',
+					display: 'flex',
+					m: 'auto',
+					p: 2,
+					flexDirection: { xs: 'row', sm: 'column' },
+					flexWrap: 'wrap',
+					justifyContent: { xs: 'space-evenly', sm: 'flex-start' },
+					alignContent: { xs: 'space-evenly', sm: 'flex-start' },
+				}}
+			>
+				{clickableIcons()}
+			</Box>
+		</>
+	)
+}
+
+export default Launchers
