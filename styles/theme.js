7a1ce6add12dcbebf5d4298b73faafcbfc4b333f
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-13 22:35:14 -0400

    revert back to 1 theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,39 +1,19 @@
-const getDesignTokens = (mode) => ({
+import { createTheme } from '@mui/material/styles'
+const theme = createTheme({
 	palette: {
-		mode,
-		...(mode === 'light'
-			? {
-					// palette values for light mode
-					primary: {
-						light: '#757ce8',
-						main: '#3f50b5',
-						dark: '#002884',
-						contrastText: '#fff',
-					},
-					secondary: {
-						light: '#ff7961',
-						main: '#f44336',
-						dark: '#ba000d',
-						contrastText: '#000',
-					},
-					background: { default: '#0D0D0D', paper: '#0D0D0D' },
-			  }
-			: {
-					// palette values for dark mode
-					primary: {
-						light: '#757ce8',
-						main: '#3f50b5',
-						dark: '#002884',
-						contrastText: '#fff',
-					},
-					secondary: {
-						light: '#ff7961',
-						main: '#f44336',
-						dark: '#ba000d',
-						contrastText: '#000',
-					},
-					background: { default: '#0D0D0D', paper: '#0D0D0D' },
-			  }),
+		primary: {
+			light: '#757ce8',
+			main: '#3f50b5',
+			dark: '#002884',
+			contrastText: '#fff',
+		},
+		secondary: {
+			light: '#ff7961',
+			main: '#f44336',
+			dark: '#ba000d',
+			contrastText: '#000',
+		},
+		background: { default: '#0D0D0D', paper: '#0D0D0D' },
 	},
 	typography: {
 		fontFamily: ['"Audiowide"', 'Lato'].join(','),
@@ -41,4 +21,4 @@ const getDesignTokens = (mode) => ({
 	},
 })
 
-export { getDesignTokens }
+export { theme }
