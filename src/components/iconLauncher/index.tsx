00fbceaeae0aee594e6d9f66705425270611547f
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-13 22:31:31 -0400

    rm

--- src/components/iconLauncher/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material'
-
-const IconLauncher = (imgSrc) => {
-	return <Box sx={{ borderRadius: { xs: '50%', sm: '2em' },  }}>
-		
-	</Box>
-}
-
-export default IconLauncher
