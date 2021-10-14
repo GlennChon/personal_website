14fb8669ba25b2fcea1b78f50e6a70e1ed62600a
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-13 22:36:46 -0400

    cleanup and organize

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-const nextConfig = {
-	webpack: (config, options) => {
-		return config
-	},
-	eslint: {
-		// ESLint managed on the workspace level
-		ignoreDuringBuilds: true,
-	},
-	images: {
-		disableStaticImages: true,
-	},
+module.exports = {
+	reactStrictMode: true,
 }
-const config = withPlugins([
-	[
-		optimizedImages,
-		{
-			optimizeImages: true,
-		},
-	],
-	nextConfig,
-])
-module.exports = config
