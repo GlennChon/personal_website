e7934a7894386a5ffbd0a96a91b9bb34dd8c05b6
glennchon <8988140+GlennChon@users.noreply.github.com>
2021-10-14 20:03:11 -0400

    cleanup

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as Desk } from './desk'
 export { default as Display } from './display'
 export { default as Keyboard } from './keyboard'
-export { default as Monitor } from './monitor'
 export { default as Launchers } from './launchers'
+export { default as Monitor } from './monitor'
 export { default as Seasonal } from './seasonal'
+
